15dd734878e7d8357773beabe9aa4fe2e7b0dcd0
Anurag <tripathi.anurag123.at@gmail.com>
2023-01-03 14:56:48 +0530

    Template render property used. Created many templates for future use.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
     path('', views.home),
     path('howItWork/', views.howItWork),
     path('careers/', views.careers),
-    path('howItWork/', views.howItWork),
     path('partnerships/', views.partnerships),
     path('blog/', views.blog),
-    path('login/', views.login),
+    path('auth/login/', views.login),
     path('downloadApp/', views.downloadApp),
+    path('pricing/', views.pricing),
+    path('checkOutForm/', views.checkOutForm),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,29 +4,29 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/base.html')
+    return render(request, 'accounts/index.html')
 
 def careers(request):
     # return HttpResponse('careers Page')
     return render(request, 'accounts/careers.html')
 
 def howItWork(request):
-    # return HttpResponse('howItWork Page')
     return render(request, 'accounts/howItWork.html')
 
 def partnerships(request):
-    # return HttpResponse('partnerships Page')
     return render(request, 'accounts/partnerships.html')
 
 def blog(request):
-    # return HttpResponse('blog Page')
     return render(request, 'accounts/blog.html')
 
 def login(request):
-    # return HttpResponse('login Page')
     return render(request, 'accounts/login.html')
 
-
 def downloadApp(request):
-    # return HttpResponse('download-app Page')
     return render(request, 'accounts/downloadApp.html')
+
+def pricing(request):
+    return render(request, 'accounts/pricing.html')
+
+def checkOutForm(request):
+    return render(request, 'accounts/checkOutForm.html')
